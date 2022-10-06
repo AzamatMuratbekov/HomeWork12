ba0ff676fe94301c2b2793a76ef15cb7365ed1f1
Melloman <lovelyboyy@mail.ru>
2022-10-06 09:17:56 +0300

    1

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Dog {
+public class Dog {
+    private String name;
+    private String type;
+    private int age;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- src/Fish.java
+++ src/Fish.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Fish {
+public class Fish {
+    private String name;
+    private String type;
+    private int size;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
